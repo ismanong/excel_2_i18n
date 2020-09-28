4846c1a77f6b0204e61de260a640a369a5776230
gao_alienware <526443791@qq.com>
2020-09-28 19:46:18 +0800

    1

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:path_provider_windows/path_provider_windows.dart';
-
+import 'package:excel/excel.dart';
 import 'dir_file_tools.dart';
 
 void main() async {
@@ -28,6 +28,8 @@ class _MyAppState extends State<MyApp> {
   String _appSupportDirectory = 'Unknown';
   String _documentsDirectory = 'Unknown';
   String _xxxxx = 'Unknown';
+  String _isExcelWeb = 'Unknown';
+  Map<String, Sheet> _excelTables = {};
 
   @override
   void initState() {
@@ -74,18 +76,69 @@ class _MyAppState extends State<MyApp> {
     });
 
     // var myDir = new Directory('D:/_work/svn资源/client/前端技术/语料包');
-    var myDir = new Directory('D:/');
-    print("Path: ${myDir.absolute.path}\n");
-    Stream<FileSystemEntity> entityList =
-        myDir.list(recursive: false, followLinks: false);
-    await for (FileSystemEntity entity in entityList) {
-      //文件、目录和链接都继承自FileSystemEntity
-      //FileSystemEntity.type静态函数返回值为FileSystemEntityType
-      //FileSystemEntityType有三个常量：
-      //Directory、FILE、LINK、NOT_FOUND
-      //FileSystemEntity.isFile .isLink .isDerectory可用于判断类型
-      print(entity.path);
+    // var myDir = new Directory('D:/');
+    // var myDir = new Directory('D:/_work/client-web-tools/i18n-excel-to-json');
+    // print("Path: ${myDir.absolute.path}\n");
+    // Stream<FileSystemEntity> entityList =
+    //     myDir.list(recursive: false, followLinks: false);
+    // await for (FileSystemEntity entity in entityList) {
+    //   //文件、目录和链接都继承自FileSystemEntity
+    //   //FileSystemEntity.type静态函数返回值为FileSystemEntityType
+    //   //FileSystemEntityType有三个常量：
+    //   //Directory、FILE、LINK、NOT_FOUND
+    //   //FileSystemEntity.isFile .isLink .isDerectory可用于判断类型
+    //   print(entity.path);
+    // }
+
+
+
+    String filePath = "D:/_work/client-web-tools/i18n-excel-to-json/i18n_web.xlsx";
+    File file = File(filePath);
+    if(file.existsSync()){
+      setState(() {
+        _isExcelWeb = '有文件';
+      });
+      var bytes = file.readAsBytesSync();
+      var excel = Excel.decodeBytes(bytes);
+      setState(() {
+        _excelTables = excel.tables;
+      });
+      for (var table in excel.tables.keys) {
+        print('sheet name: '+ table); //sheet Name
+        print('maxCols   : '+ excel.tables[table].maxCols.toString());
+        print('maxRows   : '+ excel.tables[table].maxRows.toString());
+        for (var row in excel.tables[table].rows) {
+          print("$row");
+        }
+      }
+    }else{
+      setState(() {
+        _isExcelWeb = '无文件';
+      });
     }
+  }
+
+  Widget excelListItem(List<List<dynamic>> rows){
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: List<Widget>.generate(rows.length, (index){
+        return Text('${rows[index][0]},${rows[index][1]}');
+      }),
+    );
+  }
+
+  Widget excelListContent(){
+    List<Widget> list = [];
+    _excelTables.keys.forEach((String sheetName){
+      Sheet sheet = _excelTables[sheetName];
+      List<List<dynamic>> rows = sheet.rows;
+      list.add(excelListItem(rows));
+      // print(excelListItem(rows));
+      // list.add(Text('Path Provider example app'));
+    });
+    return ListView(
+      children: list,
+    );
   }
 
   @override
@@ -103,6 +156,10 @@ class _MyAppState extends State<MyApp> {
               Text('下载目录: $_downloadsDirectory\n'),
               Text('应用程序支持目录: $_appSupportDirectory\n'),
               Text('xxxx: $_xxxxx\n'),
+              Text('是否有多语言excel: $_isExcelWeb\n'),
+              Expanded(
+                child: excelListContent(),
+              ),
             ],
           ),
         ),
